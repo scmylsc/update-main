6b54439770f65d2c5d15db3284f651787dcaadb1
scmylsc <124163547+scmylsc@users.noreply.github.com>
2023-02-04 12:05:22 +0530

    Add files via upload

--- scmylsc-main/js/sub_cakes.js
+++ scmylsc-main/js/sub_cakes.js
@@ -1,30 +1,16 @@
 function open_sub_nav(){
-        var tik = document.getElementById("checkbox");  
+        var tik = document.getElementById("checkbox1");  
         var text= document.getElementById("navid");
         var icon = document.getElementById("menu-icon");
         if(tik.checked == true){
             text.style.display = "none";
-            text.style.transition = "all 2s";
             icon.className = "fa fa-bars";
         }
-        if(tik.checked == false){
-    
-            text.style.transition = "all 2s";
+        else{
+            icon.className = "fa fa-close";
             text.style.display = "block";
-           icon.className = "fa fa-close";
-    
-        }    
+        }
+
+        
+
     }
-
-
-
-
-
-function click(){
-    location.href = '../main_html?sub_cakes.html'
-}
-
-
-function test(){
-    alert(1);
-}
